a0ea649ad9ce95e99850050d2c4ab7bf2cbd7c49
ParcheZ <popov.yakov23@yandex.ru>
2022-07-06 01:40:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/petrikirhe/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.petrikirhe;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
+
+    //С 29 по 52 это моя попытка реализовать запрос, но что делать дальше, как получить идентификатор внешней системы
+    private final OkHttpClient client = new OkHttpClient();
+    public void run() throws Exception {
+        Request request = new Request.Builder()
+                .url("https://api.tickets.yandex.net/api/agent\\\n" +
+                        " ? action=order.ticket.list\\\n" +
+                        " & auth=<string>\\\n" +
+                        " & city_id=<number>\\\n" +
+                        " & [event_id=<number>]\\\n" +
+                        " & [start_date=<string>]\\\n" +
+                        " & [end_date=<string>]")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            Headers responseHeaders = response.headers();
+            for (int i = 0; i < responseHeaders.size(); i++) {
+                System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+            }
+
+            System.out.println(response.body().string());
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Далее уже непосредственно сканирование.
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+}
